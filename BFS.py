d75f0b6ee8bdb6ac55a0660cb0d73b3ecda8a8cb
akshitha24-hash <73749951+akshitha24-hash@users.noreply.github.com>
2023-12-21 01:55:54 +0530

    Add files via upload

---
+++ BFS.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            print(node, end=' ')
+            visited.add(node)
+            queue.extend(graph[node] - visited)
+
+# Example Usage:
+graph = {
+    'A': {'B', 'C'},
+    'B': {'A', 'D', 'E'},
+    'C': {'A', 'F', 'G'},
+    'D': {'B'},
+    'E': {'B'},
+    'F': {'C'},
+    'G': {'C'}
+}
+
+print("BFS:")
+bfs(graph, 'A')
